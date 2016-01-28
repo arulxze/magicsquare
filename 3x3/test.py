d04b70202fd29579f340d1649d8f0db6070f1800
Arulx Z <arulxze@gmail.com>
2016-01-28 13:47:15 +0530

    Update test.py

--- 3x3/test.py
+++ 3x3/test.py
@@ -1,1 +1,35 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]          # Numbers involved in the magic square
+sidesum = sum(x for x in nums) / 3          # Sum of side
 
+def sum_to_n(n, size, limit=None):
+    """ Produce all lists of `size` positive integers in decreasing order
+    that add up to `n`.
+        
+        FLAGS
+        -----
+        - Remove duplicates in the list
+        - Remove numbers out of the range in the list n
+    """
+    if size == 1:
+        yield [n]
+        return
+    if limit is None:
+        limit = n
+    start = (n + size - 1) // size
+    stop = min(limit, n - size + 1) + 1
+    for i in range(start, stop):
+        for tail in sum_to_n(n - i, size - 1, i):
+            yield [i] + tail
+
+tot = []                                    # Total possibilites
+
+for side in sum_to_n(sidesum, 3):
+    if len(side) == len(set(side)):         # Check for duplicates
+        if all(x < 10 for x in side):       # Make this statement more generic
+                                            # The highest element may not be n
+                                            # FLAG: check if x is equal to any
+                                            # element in nums
+            tot.append(side)
+            
+            
+print tot                                   # FLAG: temp
